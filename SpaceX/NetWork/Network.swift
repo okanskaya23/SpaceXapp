7dda417c53390a4f50b1faf5f96cf72f3ada35d3
Okan Sarp Kaya <79059842+okanskaya23@users.noreply.github.com>
2022-05-14 11:49:20 +0300

    Base

---
+++ SpaceX/NetWork/Network.swift
@@ -0,0 +1,16 @@
+//
+//  Apollo.swift
+//  SpaceX
+//
+//  Created by Okan Sarp Kaya on 14.05.2022.
+//
+
+import Foundation
+import Apollo
+import ApolloWebSocket
+
+class Network {
+  static let shared = Network()
+
+  private(set) lazy var apollo = ApolloClient(url: URL(string: "https://api.spacex.land/graphql/")!)
+}
---
+++ SpaceX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpaceX
+//
+//  Created by Okan Sarp Kaya on 13.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var tw: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
