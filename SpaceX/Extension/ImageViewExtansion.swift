dd8a77a438e1600c853040dfbaeb47b8060b43c9
Okan Sarp Kaya <79059842+okanskaya23@users.noreply.github.com>
2022-05-16 09:43:38 +0300

    -ImageView added to detailView -Basic UnitTests -Improvments

---
+++ SpaceX/Extension/ImageViewExtansion.swift
@@ -0,0 +1,23 @@
+//
+//  ImageViewExtansion.swift
+//  SpaceX
+//
+//  Created by Okan Sarp Kaya on 16.05.2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
